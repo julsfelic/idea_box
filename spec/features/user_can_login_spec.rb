e2b6be98a097fa1fd35f19d893e4d16c11bc16e2
Julian Feliciano <julsfelic@gmail.com>
2016-02-27 13:09:19 -0700

    Add sad path for a user loggin in

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -17,4 +17,27 @@ RSpec.feature "User can login" do
     end
     expect(page).to_not have_link("Login")
   end
+
+  context "with an invalid username" do
+    scenario "they see an error message" do
+      user = FactoryGirl.create(:user)
+      invalid_username = "#{user.username} - invalid"
+
+      visit root_path
+      click_link "Login"
+      fill_in "Username", with: invalid_username
+      fill_in "Password", with: "password"
+      click_button "Login"
+
+      expect(page).to_not eq(user_path(user))
+      expect(page).to_not have_content(user.username)
+      within(".site-header") do
+        expect(page).to have_link("Login")
+      end
+      expect(page).to_not have_link("Logout")
+      within(".flash-error") do
+        expect(page).to have_content("Invalid login info")
+      end
+    end
+  end
 end
