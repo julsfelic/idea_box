df10a7e7ff08369d36692d33ed853ee6d9ce0bc3
Julian Feliciano <julsfelic@gmail.com>
2016-02-27 11:49:57 -0700

    Allow users to create an account
    Give users the ability to create an account and see their dashboard

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -35,7 +35,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'pry-rails'
-  gem 'rspec-rails', '~> 3.0'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
 end
 
 group :development do
---
+++ spec/features/user_can_create_an_account_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature 'User can create an account' do
+  scenario 'they see their dashboard with a welcome message' do
+    username = 'julsfelic'
+
+    visit root_path
+    click_link 'Register'
+    fill_in 'Username', with: username
+    fill_in 'Password', with: 'password'
+    click_button 'Create Account'
+
+    user = User.last
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content(username)
+    within('.flash-success') do
+      expect(page).to have_content('Welcome to the Idea Box Project')
+    end
+  end
+end
